e5f2a359cecd65ebcbf8be84287cedc16c259314
kKATO_lab <di.aps53@gmail.com>
2016-09-10 14:41:59 +0900

    add RF regression

--- regression.py
+++ regression.py
@@ -7,6 +7,7 @@
 
 import numpy as np
 from sklearn import linear_model,svm
+from sklearn.ensemble import RandomForestRegressor
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -23,26 +24,44 @@ class Regression:
         :param coef: 偏回帰係数
         :param intercept: 誤差
         """
+        self.data = dataFrame
         self.explanatory_columns = explanatory_columns
         self.criterion_column = criterion_column
         self.explanatory_variables = dataFrame[self.explanatory_columns].as_matrix()
         self.criterion_variables = dataFrame[self.criterion_column].as_matrix()
 
     def linear_regression(self):
-        self.clf = linear_model.LinearRegression(fit_intercept=True,normalize=False,
+        self.rm = linear_model.LinearRegression(fit_intercept=True,normalize=False,
                                                  copy_X=True,n_jobs=-1)
         """
         :param fit_intercept: Falseにすると切片を求めない(原点を通る場合に有効)
         :param normalize: Trueにすると説明変数を正規化する
         :param copy_X: メモリないでデータを複製するかどうか
         """
-        self.clf.fit(self.explanatory_variables,self.criterion_variables)
+        self.rm.fit(self.explanatory_variables,self.criterion_variables)
         self.coef = pd.DataFrame({"Name":self.explanatory_columns,
-                                  "Coefficients":self.clf.coef_})
-        self.intercept = self.clf.intercept_
-        self.predict = lambda x: self.clf.predict(x)
+                                  "Coefficients":self.rm.coef_})
+        self.intercept = self.rm.intercept_
+        self.predict = lambda x: self.rm.predict(x)
 
     def svr(self):
-        self.clf = svm.SVR(kernel='rbf',C=1e3,gamma=0.1)
-        self.clf.fit(self.explanatory_variables,self.criterion_variables)
-        self.predict = lambda x: self.clf.predict(x)
+        self.rm = svm.SVR(kernel='rbf',C=1,gamma=0.1)
+        self.rm.fit(self.explanatory_variables,self.criterion_variables)
+        self.predict = lambda x: self.rm.predict(x)
+
+    def random_forest_regression(self):
+        self.rm = RandomForestRegressor(n_estimators=100,criterion="mse",
+                                   max_features="auto",n_jobs=-1)
+        self.rm.fit(self.explanatory_variables,self.criterion_variables)
+
+        features_importance_rank = np.argsort(self.rm.feature_importances_)[::-1]
+        features_importance_value = self.rm.feature_importances_[features_importance_rank]
+        features_importance_key = self.data[features_importance_rank].keys()
+        importance = pd.DataFrame(
+            {
+                "key": features_importance_key,
+                "value": features_importance_value
+            }
+        )
+        sns.barplot(x='value', y='key', data=importance)
+        plt.show()
--- sample.py
+++ sample.py
@@ -26,3 +26,5 @@ if __name__ == "__main__":
     plt.plot(model.predict(data[["SepalLength", "SepalWidth", "PetalLength"]]), "g")
     plt.show()
 
+    model.random_forest_regression()
+
